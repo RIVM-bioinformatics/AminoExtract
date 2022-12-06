ae0ec18951041542fe3c3f4f539be2d711e6f022
ids-bioinformatics <81251468+ids-bioinformatics@users.noreply.github.com>
2022-12-06 12:22:32 +0100

    chore(main): release 0.2.1

--- AminoExtract/__init__.py
+++ AminoExtract/__init__.py
@@ -1,7 +1,7 @@
 import contextlib
 
 __prog__ = "AminoExtract"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 # use contextlib to suppress the ImportError
 # This may occur when this file is imported in setup.py as dependencies are not yet installed
